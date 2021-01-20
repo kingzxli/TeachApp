a92bfbf0054e155014ec424724d46f7a23febd0c
king <li.zhengxue@infox.com.cn>
2021-01-20 17:31:36 +0800

    fix

--- TeachDemo/src/main/java/com/song/service/impl/TeacherServiceImpl.java
+++ TeachDemo/src/main/java/com/song/service/impl/TeacherServiceImpl.java
@@ -154,6 +154,7 @@ public class TeacherServiceImpl implements TeacherService{
 
 	@Override
 	public List<Teacher> selectByNamePro(Teacher teacher) {
+		// TODO Auto-generated method stub
 		return teacherMapper.selectByNamePro(teacher);
 	}
 
